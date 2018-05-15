d4807b7312a5b17de122b8610b3cfd93e3e33bed
ygx <sinsyet@163.com>
2018-05-15 15:52:43 +0800

    add

--- JavaSample/src/main/java/com/example/javasample/App.java
+++ JavaSample/src/main/java/com/example/javasample/App.java
@@ -1,4 +1,12 @@
 package com.example.javasample;
 
+import java.util.Locale;
+
 public class App {
+    public static void main(String[] args) {
+        String mResp = "{\"t\":%d,\"mid\":%d,\"msg\":{" +
+                "\"host\":\"%s\",\"port\":%d"
+                + "}}";
+        System.out.println();
+    }
 }
